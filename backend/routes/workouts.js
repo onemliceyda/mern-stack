43baca99341545f6620322cff19643ed24bf84ff
Ceyda <ceydaonemli2@gmail.com>
2022-12-19 21:22:04 +0300

    controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,38 +1,28 @@
 const express = require("express")
-const Cluster0 = require("../models/workoutModel")
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,updateWorkout
+} = require("../controllers/workoutController")
+
 //create a router
 const router = express.Router()
 
 //GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all workouts" })
-})
+router.get("/", getWorkouts)
 
 //GET a single workouts
 
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single workout" })
-})
+router.get("/:id",getWorkout)
 
 //POST a new workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body
-  try {
-    const workout = await Cluster0.create({ title, load, reps })
-    res.status(200).json(workout)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
-})
+router.post("/", createWorkout)
 
 //DELETE a new workout
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a new workout" })
-})
+router.delete("/:id",deleteWorkout)
 
 //UPDATE a new workout
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "UPDATE a new workout" })
-})
+router.patch("/:id",updateWorkout)
 
 module.exports = router
