b38e24d9dcf08592de5e4166dbf9c15713e62196
Ceyda <ceydaonemli2@gmail.com>
2022-12-22 17:28:31 +0300

    signing tokens

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,7 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const validator = require("validator")
+
 const Schema = mongoose.Schema
 const userSchema = new Schema({
   email: {
