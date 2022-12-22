754b889dadb8831f7c0a58fbbbb175c028fc7eae
Ceyda <ceydaonemli2@gmail.com>
2022-12-22 16:33:24 +0300

    email password validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,39 +1,47 @@
 //it's just gonna have an e-mail and password fields.
 
-const mongoose=require("mongoose")
-const bcrypt=require("bcrypt")
-const Schema=mongoose.Schema
-const userSchema=new Schema(
-    {
-        email:{
-            type:String,
-            required:true,
-            unique:true
-        },
-        password:{
-            type:String,
-            required:true,
-        },
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+//static signup method
+userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled")
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid")
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough")
+  }
 
-    })
-   //static signup method
-   userSchema.statics.signup=async function (email,password){
-    const exists=await this.findOne({email})
-    if(exists){
-        throw Error("Email already in use")
-    }
+  const exists = await this.findOne({ email })
+  if (exists) {
+    throw Error("Email already in use")
+  }
 
-    //bcrypt hashing the passwords
+  //bcrypt hashing the passwords
 
-    const salt=await bcrypt.genSalt(10)
-    const hash=await bcrypt.hash(password,salt)
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
 
-    //create method is create a new document for us 
-    //we're gonna pass an object to this document
+  //create method is create a new document for us
+  //we're gonna pass an object to this document
 
-    const user=await this.create({email,password:hash})
-    return user
-
-
-   }
-    module.exports=mongoose.model("User",userSchema)
+  const user = await this.create({ email, password: hash })
+  return user
+}
+module.exports = mongoose.model("User", userSchema)
